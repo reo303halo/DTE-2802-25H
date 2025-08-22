9145dd41a9634b9b060ee6a40d050cce80844c59
Roy Espen Olsen <roy-eo@hotmail.com>
2025-08-22 20:04:51 +0200

    Repository pattern

---
+++ MVC/GarageMVC_Repository/GarageMVC/Controllers/CarController.cs
@@ -0,0 +1,38 @@
+using GarageMVC.Models.Entities;
+using GarageMVC.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarageMVC.Controllers;
+
+public class CarController(ICarRepository repository) : Controller
+{
+    // GET : Read
+    public IActionResult Index()
+    {
+        var cars = repository.GetCars();
+        
+        return View(cars);
+    }
+    
+    // GET : Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    // POST : Create
+    [HttpPost]
+    public IActionResult Create([Bind("LicenseNumber,Make,Model,Year")] Car car)
+    {
+        try
+        {
+            if (!ModelState.IsValid) return View();
+            repository.Save(car);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        return RedirectToAction(nameof(Index));
+    }
+}
